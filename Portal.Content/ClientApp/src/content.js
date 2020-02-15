3299d78eda22ea551fd370e8d27e16f8a6c2fa45
Sercan Boyraz <serca@BIS-1016244860.saglik.lokal>
2020-02-16 00:40:36 +0300

    wifi close

--- Portal.Content/ClientApp/src/content.js
+++ Portal.Content/ClientApp/src/content.js
@@ -1,6 +1,11 @@
 import React from 'react'
-import { ListGroup, InputGroup, Button, FormControl, Modal } from 'react-bootstrap'
+import { ListGroup, InputGroup, Button, FormControl, Modal, OverlayTrigger, Tooltip } from 'react-bootstrap'
+import { SaveOutlined, LoopOutlined, WifiOutlined, PowerSettingsNew, DoneAll, HighlightOff } from '@material-ui/icons';
+
 const axios = require('axios');
+const electron = window.require('electron');
+const fs = electron.remote.require('fs');
+const ipcRenderer = electron.ipcRenderer;
 // Make a request for a user with a given ID
 
 export default class Content extends React.Component {
@@ -8,6 +13,7 @@ export default class Content extends React.Component {
     state = {
         permissions: null,
         show: false,
+        showShutdown: false,
         ssid: "",
         password: ""
     }
@@ -19,6 +25,8 @@ export default class Content extends React.Component {
 
     componentDidMount() {
         this.setState({ permissions: this.props.permission });
+        this.setState({ wifiList: JSON.parse(localStorage.getItem("wifis")) });
+
         var getLocalStorageUserId = localStorage.getItem('userPortalId');
         if (getLocalStorageUserId && !this.props.permission) {
             if (getLocalStorageUserId) {
@@ -33,7 +41,7 @@ export default class Content extends React.Component {
 
         var webview = document.getElementById('webview');
         webview.addEventListener('dom-ready', function () {
-            webview.insertCSS(".modal-body.blockElement,.lgbtn,.relative,.grid_4,.footer-list,.footerText,.gradient-gold,.bottom-followus,.loggedOut,.admatic_interstitial_iframe_content_main,.page-homepage-index__container-widget,.page-homepage-index__container-widget--social-posts,.widget-footer,.widget-nesine-most-played-coupons,.widget-nesine-most-played-coupons--desktop,.logos,.widget,.footer-nav,.footer-links,[class^='Stage_Rectangle'],.widget-legacy-link-banner,.widget-social-post__follow-button,.adform-adbox,.admatic_interstitial_logo_area_span,iframe,.c,.b,.a,.medyanet-ad-models-pageskin,.mpu,.userbox,.lgnform { display: none !important}");
+            webview.insertCSS(".popupLoginMain,.footerContainer,.headerLogin,.modal-body.blockElement,.lgbtn,.relative,.grid_4,.footer-list,.footerText,.gradient-gold,.bottom-followus,.loggedOut,.admatic_interstitial_iframe_content_main,.page-homepage-index__container-widget,.page-homepage-index__container-widget--social-posts,.widget-footer,.widget-nesine-most-played-coupons,.widget-nesine-most-played-coupons--desktop,.logos,.widget,.footer-nav,.footer-links,[class^='Stage_Rectangle'],.widget-legacy-link-banner,.widget-social-post__follow-button,.adform-adbox,.admatic_interstitial_logo_area_span,iframe,.c,.b,.a,.medyanet-ad-models-pageskin,.mpu,.userbox,.lgnform { display: none !important}");
             webview.insertCSS(".coupon__bottom-line  { display:block; } ");
             webview.insertCSS(".coupon__select-input  { padding-left: 45px; text-align:right !important; }");
             webview.insertCSS(".coupon__select-box  { width: 100%; } ");
@@ -54,7 +62,7 @@ export default class Content extends React.Component {
     componentWillUpdate() {
         var webview = document.getElementById('webview');
         webview.addEventListener('dom-ready', function () {
-            webview.insertCSS(".modal-body.blockElement,.lgbtn,.relative,.grid_4,.footer-list,.footerText,.gradient-gold,.bottom-followus,.loggedOut,.admatic_interstitial_iframe_content_main,.page-homepage-index__container-widget,.page-homepage-index__container-widget--social-posts,.widget-footer,.widget-nesine-most-played-coupons,.widget-nesine-most-played-coupons--desktop,.logos,.widget,.footer-nav,.footer-links,[class^='Stage_Rectangle'],.widget-legacy-link-banner,.widget-social-post__follow-button,.adform-adbox,.admatic_interstitial_logo_area_span,iframe,.c,.b,.a,.medyanet-ad-models-pageskin,.mpu,.userbox,.lgnform { display: none !important}");
+            webview.insertCSS(".popupLoginMain,.footerContainer,.headerLogin,.modal-body.blockElement,.lgbtn,.relative,.grid_4,.footer-list,.footerText,.gradient-gold,.bottom-followus,.loggedOut,.admatic_interstitial_iframe_content_main,.page-homepage-index__container-widget,.page-homepage-index__container-widget--social-posts,.widget-footer,.widget-nesine-most-played-coupons,.widget-nesine-most-played-coupons--desktop,.logos,.widget,.footer-nav,.footer-links,[class^='Stage_Rectangle'],.widget-legacy-link-banner,.widget-social-post__follow-button,.adform-adbox,.admatic_interstitial_logo_area_span,iframe,.c,.b,.a,.medyanet-ad-models-pageskin,.mpu,.userbox,.lgnform { display: none !important}");
             webview.insertCSS(".coupon__bottom-line  { display:block; } ");
             webview.insertCSS(".coupon__select-input  { padding-left: 45px; text-align:right !important; }");
             webview.insertCSS(".coupon__select-box  { width: 100%; } ");
@@ -75,7 +83,7 @@ export default class Content extends React.Component {
     componentDidUpdate() {
         var webview = document.getElementById('webview');
         webview.addEventListener('dom-ready', function () {
-            webview.insertCSS(".modal-body.blockElement,.lgbtn,.relative,.grid_4,.footer-list,.footerText,.gradient-gold,.bottom-followus,.loggedOut,.admatic_interstitial_iframe_content_main,.page-homepage-index__container-widget,.page-homepage-index__container-widget--social-posts,.widget-footer,.widget-nesine-most-played-coupons,.widget-nesine-most-played-coupons--desktop,.logos,.widget,.footer-nav,.footer-links,[class^='Stage_Rectangle'],.widget-legacy-link-banner,.widget-social-post__follow-button,.adform-adbox,.admatic_interstitial_logo_area_span,iframe,.c,.b,.a,.medyanet-ad-models-pageskin,.mpu,.userbox,.lgnform { display: none !important}");
+            webview.insertCSS(".popupLoginMain,.footerContainer,.headerLogin,.modal-body.blockElement,.lgbtn,.relative,.grid_4,.footer-list,.footerText,.gradient-gold,.bottom-followus,.loggedOut,.admatic_interstitial_iframe_content_main,.page-homepage-index__container-widget,.page-homepage-index__container-widget--social-posts,.widget-footer,.widget-nesine-most-played-coupons,.widget-nesine-most-played-coupons--desktop,.logos,.widget,.footer-nav,.footer-links,[class^='Stage_Rectangle'],.widget-legacy-link-banner,.widget-social-post__follow-button,.adform-adbox,.admatic_interstitial_logo_area_span,iframe,.c,.b,.a,.medyanet-ad-models-pageskin,.mpu,.userbox,.lgnform { display: none !important}");
             webview.insertCSS(".coupon__bottom-line  { display:block; } ");
             webview.insertCSS(".coupon__select-input  { padding-left: 45px; text-align:right !important; }");
             webview.insertCSS(".coupon__select-box  { width: 100%; } ");
@@ -111,27 +119,52 @@ export default class Content extends React.Component {
         }
     }
 
-    handleClose = () => this.setState({ show: false });
-    handleShow = () => this.setState({ show: true });
+    handleClose = () => {
+        this.setState({ show: false });
+    }
+
+    handleCloseShutdown = () => {
+        this.setState({ showShutdown: false });
+    }
+
+    handleShow = () => {
+        this.setState({ show: true });
+    }
+
     handleSave = () => {
         localStorage.setItem("SSID", this.state.ssid);
         localStorage.setItem("Password", this.state.password);
-        this.handleClose();
+        ipcRenderer.invoke('set-wifi-names', null).then((result) => {
+            this.handleClose();
+        })
     }
 
     refresh = (e) => {
         e.preventDefault();
-        //window.location.reload();
+        window.location.reload();
     }
 
-    // setWifiPassword = (pass) => this.setState({ password: pass });
-    // setWifiSSID = (id) => this.setState({ ssid: id });
+    refreshWifi = () => {
+        ipcRenderer.invoke('get-wifi-names', null).then((result) => {
+            console.log(result);
+            this.setState({ wifiList: result });
+        })
+    }
+
+    shutdown = () => {
+        ipcRenderer.invoke('shutdown_event', null).then((result) => {
+
+        })
+    }
+
+    setWifiPassword = (pass) => this.setState({ password: pass });
+    setWifiSSID = (id) => this.setState({ ssid: id });
+
     render() {
         const { permissions } = this.state;
-        // const wifiList = JSON.parse(localStorage.getItem("wifis"));
         return (
             <div>
-                <header className="main-head">
+                <header className="main-head" >
                     <nav className="head-nav">
                         <ul className="menu">
                             {
@@ -146,51 +179,123 @@ export default class Content extends React.Component {
                         </ul>
                     </nav>
                     <div>
-                        {/* <div style={{ padding: 3, width: '100%', position: 'absolute', bottom: 1 }}>
-                        <Button variant="dark" onClick={e => this.refresh(e)} id="refreshButton" >
-                            y
-                    </Button> */}
-                        {/* <Button variant="dark" onClick={this.handleShow} style={{ padding: 3, width: '100%', position: 'absolute', bottom: 1 }} >
-                        wifi
-                    </Button> */}
-                        {/* </div> */}
-                        {/* <Modal show={this.state.show} onHide={this.handleClose}>
-                        <Modal.Header closeButton>
-                            <Modal.Title>Wifi</Modal.Title>
-                        </Modal.Header>
-                        <Modal.Body>
-                            <ListGroup defaultActiveKey="#link1">
-                                {
-                                    wifiList &&
-                                    wifiList.map((data) =>
-                                        <ListGroup.Item action style={{ fontSize: 12, padding: 0, margin: 0, height: 39 }} onSelect={() => this.setWifiSSID(data.ssid)}>
-                                            <InputGroup className="mb-3">
-                                                <InputGroup.Prepend>
-                                                    <InputGroup.Text id="basic-addon3">
-                                                        {
-                                                            data.ssid
-                                                        }
-                                                    </InputGroup.Text>
-                                                </InputGroup.Prepend>
-                                                <FormControl id={data.ssid} aria-describedby="basic-addon3" onChange={(e) => this.setWifiPassword(e.target.value)} onClick={() => this.setWifiSSID(data.ssid)} />
-                                            </InputGroup>
-                                        </ListGroup.Item>
-                                    )
-                                }
-                            </ListGroup>
-                        </Modal.Body>
-                        <Modal.Footer>
-                            <Button variant="secondary" onClick={this.handleClose}>
-                                Close
-                            </Button>
-                            <Button variant="primary" onClick={() => this.handleSave()}>
-                                Save Changes
-                            </Button>
-                        </Modal.Footer>
-                    </Modal> */}
+                        <div className="speedButton" >
+                            <OverlayTrigger
+                                key="top"
+                                placement="top"
+                                overlay={
+                                    <Tooltip id={`tooltip-PowerSettingsNew`}><strong>Kapat</strong></Tooltip>
+                                }>
+                                <Button variant="dark" onClick={e => this.setState({ showShutdown: true })} style={{ marginRight: 5 }} >
+                                    <PowerSettingsNew ></PowerSettingsNew>
+                                </Button>
+                            </OverlayTrigger>
+
+                            <OverlayTrigger
+                                key="top"
+                                placement="top"
+                                overlay={
+                                    <Tooltip id={`tooltip-LoopOutlined`}><strong>Yenile</strong></Tooltip>
+                                }>
+                                <Button variant="dark" onClick={e => this.refresh(e)} id="refreshButton" style={{ marginRight: 5 }} >
+                                    <LoopOutlined></LoopOutlined>
+                                </Button>
+
+                            </OverlayTrigger>
+
+                            <OverlayTrigger
+                                key="top"
+                                placement="top"
+                                overlay={
+                                    <Tooltip id={`tooltip-WifiOutlined`}><strong>Wifi</strong></Tooltip>
+                                }>
+                                <Button variant="dark" onClick={() => this.handleShow()} >
+                                    <WifiOutlined></WifiOutlined>
+                                </Button>
+
+                            </OverlayTrigger>
+                        </div>
+                        <Modal show={this.state.show} onHide={this.handleClose}>
+                            <Modal.Header closeButton>
+                                <Modal.Title>Kablosuz İnternet(Wifi)</Modal.Title>
+                            </Modal.Header>
+                            <Modal.Body>
+                                <ListGroup defaultActiveKey="#link1">
+                                    {
+                                        this.state.wifiList &&
+                                        this.state.wifiList.map((data) =>
+                                            <ListGroup.Item key={data.ssid} action style={{ fontSize: 12, padding: 0, margin: 0, height: 39 }} onSelect={() => this.setWifiSSID(data.ssid)}>
+                                                <InputGroup className="mb-3">
+                                                    <InputGroup.Prepend>
+                                                        <InputGroup.Text id="basic-addon3" style={{ width: 250 }}>
+                                                            {
+                                                                data.ssid
+                                                            }
+                                                        </InputGroup.Text>
+                                                    </InputGroup.Prepend>
+                                                    <FormControl id={data.ssid} type="password" aria-describedby="basic-addon3" onChange={(e) => this.setWifiPassword(e.target.value)} onClick={() => this.setWifiSSID(data.ssid)} />
+                                                </InputGroup>
+                                            </ListGroup.Item>
+                                        )
+                                    }
+                                </ListGroup>
+                            </Modal.Body>
+                            <Modal.Footer>
+                                <OverlayTrigger
+                                    key="top"
+                                    placement="top"
+                                    overlay={
+                                        <Tooltip id={`tooltip-LoopOutlineds`}><strong>Wifi Tara</strong></Tooltip>
+                                    }>
+                                    <Button variant="secondary" onClick={() => this.refreshWifi()} style={{ position: "absolute", left: 10, bottom: 11 }}>
+                                        <LoopOutlined></LoopOutlined>
+                                    </Button>
+                                </OverlayTrigger>
+                                <OverlayTrigger
+                                    key="top"
+                                    placement="top"
+                                    overlay={
+                                        <Tooltip id={`tooltip-DoneAlls`}><strong>Kaydet</strong></Tooltip>
+                                    }>
+                                    <Button variant="primary" onClick={() => this.handleSave()}>
+                                        <DoneAll></DoneAll>
+                                    </Button>
+                                </OverlayTrigger>
+                            </Modal.Footer>
+                        </Modal>
+                        <Modal show={this.state.showShutdown} onHide={this.handleCloseShutdown}>
+                            <Modal.Header closeButton>
+                                <Modal.Title>Kapat</Modal.Title>
+                            </Modal.Header>
+                            <Modal.Body>
+                                <p>Şansder Platformu kapatmak ister misiniz?</p>
+                            </Modal.Body>
+                            <Modal.Footer>
+                                <OverlayTrigger
+                                    key="top"
+                                    placement="top"
+                                    overlay={
+                                        <Tooltip id={`tooltip-HighlightOffs`}><strong>İptal Et</strong></Tooltip>
+                                    }>
+                                    <Button variant="secondary" onClick={() => this.handleCloseShutdown()} >
+                                        <HighlightOff></HighlightOff>
+                                    </Button>
+                                </OverlayTrigger>
+                                <OverlayTrigger
+                                    key="top"
+                                    placement="top"
+                                    overlay={
+                                        <Tooltip id={`tooltip-PowerSettingsNews`}><strong>Kapat</strong></Tooltip>
+                                    }>
+                                    <Button variant="primary" onClick={() => this.shutdown()}>
+                                        <PowerSettingsNew></PowerSettingsNew>
+                                    </Button>
+                                </OverlayTrigger>
+                            </Modal.Footer>
+                        </Modal>
                     </div>
                 </header>
-            </div>
+            </div >
         )
     }
 }
--- Portal.Content/Electron/main.js
+++ Portal.Content/Electron/main.js
@@ -6,9 +6,11 @@ const fs = require("fs");
 const { net } = require('electron')
 // const fetch = require('electron-main-fetch');
 const fetch = require('node-fetch');
-
+const { ipcMain } = require('electron')
 const Store = require('./store.js');
 var wifi = require("node-wifi");
+const shutdown = require('electron-shutdown-command');
+
 
 
 
@@ -84,44 +86,85 @@ app.on('ready', () => {
   });
 
 
-  // wifi.init({ iface: null });
-  // wifi.scan(function (err, networks) {
-  //   console.log(networks);
-  //   mainWindow.webContents.executeJavaScript('localStorage.setItem("wifis",JSON.stringify(' + JSON.stringify(networks) + '));', true)
-  // });
+  wifi.init({ iface: null });
 
-  // mainWindow.webContents.executeJavaScript('localStorage.getItem("SSID");', true)
-  //   .then((result) => {
-  //     console.log(result);
-  //     mainWindow.webContents.executeJavaScript('localStorage.getItem("Password");', true)
-  //       .then((result2) => {
-  //         console.log(result2);
-  //         wifi.connect({ ssid: result, password: result2 }, function (err) {
-  //           if (err) {
-  //             console.log(err);
-  //           } else
-  //             console.log("Connected");
-  //         });
-  //       })
-  //   })
+  wifi.scan().then(function (networks) {
+    mainWindow.webContents.executeJavaScript('localStorage.setItem("wifis",JSON.stringify(' + JSON.stringify(networks) + '));', true)
+    console.log(networks);
+  })
 
-  //mainWindow.loadURL('https://content.boykaf.xyz/');
+  mainWindow.webContents.executeJavaScript('localStorage.getItem("SSID");', true)
+    .then((result) => {
+      mainWindow.webContents.executeJavaScript('localStorage.getItem("Password");', true)
+        .then((result2) => {
+          wifi.connect({ ssid: result, password: result2 }, function (err) {
+            if (err) {
+              console.log(err);
+            } else
+              console.log("Connected");
+          });
+        })
+    })
+
+  mainWindow.webContents.executeJavaScript('document.getElementsByTagName("BODY")[0].innerHTML +=  "<div id="lockdiv" style="display: block;position:absolute;top: 0;left: 0;height: 100%;right: 0;width: 100%;"></div>"; ', true)
+
+  // mainWindow.loadURL('https://content.boykaf.xyz/');
   mainWindow.loadURL('http://localhost:3000/');
   mainWindow.reload();
   const menu = Menu.buildFromTemplate(template);
-  Menu.setApplicationMenu(menu);
+  Menu.setApplicationMenu(null);
 });
+
+ipcMain.handle('get-wifi-names', async (event, someArgument) => {
+  console.log("get-wifi-names") // prints "ping"
+  var resultData = await wifi.scan().then((function (networks) {
+    console.log(networks);
+    mainWindow.webContents.executeJavaScript('localStorage.setItem("wifis",JSON.stringify(' + JSON.stringify(networks) + '));')
+    return networks;
+  }));
+  return resultData;
+})
+
+ipcMain.handle('set-wifi-names', async (event, someArgument) => {
+  mainWindow.webContents.executeJavaScript('localStorage.getItem("SSID");', true)
+    .then((result) => {
+      console.log(result);
+      mainWindow.webContents.executeJavaScript('localStorage.getItem("Password");', true)
+        .then((result2) => {
+          console.log(result2);
+          wifi.connect({ ssid: result, password: result2 }, function (err) {
+            if (err) {
+              console.log(err);
+            } else
+              console.log("Connected");
+          });
+        })
+    })
+  return "";
+})
+
+ipcMain.handle('shutdown_event', async (event, someArgument) => {
+  shutdown.shutdown();
+  return "";
+})
 
 app.on('web-contents-created', (e, contents) => {
   e.preventDefault();
+  let child;
   if (contents.getType() == 'webview') {
+    mainWindow.webContents.executeJavaScript('document.getElementsByTagName("BODY")[0].innerHTML +=  "<div id="lockdiv" style="display: block;position:absolute;top: 0;left: 0;height: 100%;right: 0;width: 100%;"></div>"; ', true)
     contents.on('new-window', (e, url) => {
       e.preventDefault();
 
-      let child = new BrowserWindow({ parent: mainWindow, width: 900, height: 900, modal: true, show: false, isNormal: true, fullscreen: false, fullscreenable: false, });
+      child = new BrowserWindow({ parent: mainWindow, width: 900, height: 900, modal: true, show: false, isNormal: true, fullscreen: false, fullscreenable: false, });
       child.loadURL(url);
       child.once('ready-to-show', () => {
-        child.show();
+        try {
+          child.show();
+        }
+        catch{
+
+        }
       });
 
       fetch('https://content.boykaf.xyz/css.json')
@@ -131,17 +174,15 @@ app.on('web-contents-created', (e, contents) => {
             child.webContents.insertCSS(json);
           });
         });
-
       e.preventDefault();
     })
   }
 })
 
-
-// Quit when all windows are closed.
 app.on('window-all-closed', () => {
   data = {
     bounds: mainWindow.getBounds()
   };
   app.quit();
 });
+
